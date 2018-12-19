c1f8f0c4d285c112da47a0b686ffbbf66e1d2768
reemmostafa <reem.mostafa.48@gmail.com>
2018-12-19 14:49:34 +0200

    show likes and comments in profile page

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,24 +1,37 @@
-import { PostsService } from './../../app/services/posts/posts.service';
-import { Post } from './../../app/services/posts/post.model';
-import { User } from './../../app/services/auth/user.model';
-import { AuthService } from './../../app/services/auth/auth.service';
-import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { ModalController } from "ionic-angular";
+import { PostsService } from "./../../app/services/posts/posts.service";
+import { Post } from "./../../app/services/posts/post.model";
+import { User } from "./../../app/services/auth/user.model";
+import { AuthService } from "./../../app/services/auth/auth.service";
+import { Component } from "@angular/core";
+import { IonicPage } from "ionic-angular";
 
 @IonicPage()
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
+  selector: "page-profile",
+  templateUrl: "profile.html"
 })
 export class ProfilePage {
   user: User;
   posts: Post[];
-  constructor(private authService: AuthService, private postsService: PostsService) {
-  }
+  constructor(
+    private authService: AuthService,
+    private postsService: PostsService,
+    private modalCtrl: ModalController
+  ) {}
 
   ionViewWillLoad() {
     this.user = this.authService.getUser();
     this.posts = this.postsService.getPosts();
   }
 
+  showLikes(id: number) {
+    const modal = this.modalCtrl.create("likes-page", { id: id });
+    modal.present();
+  }
+
+  showComments(id: number) {
+    const modal = this.modalCtrl.create("comments-page", { id: id });
+    modal.present();
+  }
 }
